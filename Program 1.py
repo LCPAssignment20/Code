40245f57d7e1109b94025c94157d8929e0c6a771
jonas <jonas.eb@arcor.de>
2020-03-25 21:44:06 +0100

    25.3. 21:40

---
+++ Program 1.py
@@ -0,0 +1,11 @@
+lst = []
+n = input("Welcome to Buzzword Bingo! You will now be asked for 25 words that fit a theme:")
+
+with open("Bingo.txt", "w") as myfile:
+    for i in range(0, 25):
+        word = (input())
+
+        lst.append(word)
+
+with open("Bingo.txt", "r") as Book:
+    print(lst[1])
