52ba4a4f62fe3d0190113e14de02dab5605ec447
jonas <jonas.eb@arcor.de>
2020-03-25 22:26:18 +0100

    25.3. 22:26

--- Program1.py
+++ Program1.py
@@ -1,9 +1,9 @@
 lst = []
 
-n = input("Welcome to Buzzword Bingo! You will now be asked for 25 words that fit a theme:")
+n = input("Welcome to Buzzword Bingo! You will now be asked for 30 words that fit a theme:")
 
 with open("Bingo.txt", "w") as myfile:
-    for i in range(0, 25):
+    for i in range(0, 30):
         word = (input())
 
         lst.append(word)
--- Program2.py
+++ Program2.py
@@ -1,3 +1,5 @@
 import Program1
+import random
 
-with open("Bingo.txt", "r") as Book:
+with open("Bingo.txt", "r") as Hi:
+    print(random.sample(Program1.lst,25))
