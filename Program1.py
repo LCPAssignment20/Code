636901e36873c92db06b5b6add8f8b21a8cf7417
jonas <jonas.eb@arcor.de>
2020-03-26 11:15:25 +0100

    26.3. 11:15

--- Program1.py
+++ Program1.py
@@ -1,12 +1,12 @@
 #creating a list
-lst = []
+lst = ['Alfa Romeo', 'Aston Martin', 'Audi', 'Bentley', 'BMW', 'Bugatti', 'Cadillac', 'Chevrolet', 'Chrysler', 'Citroen', 'Corvette','DAF', 'Dacia', 'Daewoo', 'Daihatsu', 'Datsun', 'De Lorean', 'Dino', 'Dodge', 'Farboud', 'Ferrari', 'Ford', 'Honda', 'Hummer', 'Hyundai', 'Jaguar', 'Jeep', 'KIA', 'Koenigsegg', 'Lada', 'Lamborghini', 'Lancia', 'Land Rover', 'Lexus', 'Ligier', 'Lincoln', 'Lotus', 'Martini', 'Maserati', 'Maybach', 'Mazda', 'Mclaren', 'Mercedes-Benz', 'Mini', 'Mitsubishi', 'Nissan', 'Noble', 'Opel', 'Peugot', 'Pontiac', 'Porsche', 'Renault', 'Rolls-Royce', 'Rover', 'Saab', 'Seat', 'Skoda', 'Smart', 'Spyker', 'Subaru', 'Suzuki', 'Toyota', 'Vauxhall', 'Volkswagen', 'Volvo']
 
-n = input("Welcome to Buzzword Bingo! You will now be asked for 30 words that fit a theme:")
+n = input("Welcome to Buzzword Bingo! You will now be asked for 10 words that fit the theme Car Brands:")
 
 #opening file and writing in it
 with open("Bingo.txt", "w") as myfile:
 #loop that asks for 30 words that are appended to the list
-    for i in range(0, 30):
+    for i in range(0, 10):
         word = (input())
 
         lst.append(word)
